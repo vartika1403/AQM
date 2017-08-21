1ba40f8fcb01823e8e45b4dfccd561b6ce1d1eb6
vartika1403 <vartika.1403@gmail.com>
2017-08-21 19:35:07 +0530

    server socket connection added

---
+++ app/src/main/java/com/example/admin/aqm/ServerActivity.java
@@ -0,0 +1,105 @@
+package com.example.admin.aqm;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ServerActivity extends AppCompatActivity {
+    private static final String LOG_TAG = ServerActivity.class.getSimpleName();
+    private ServerSocket serverSocket;
+    Handler updateConversationHandler;
+    Thread serverThread = null;
+    public static final int SERVERPORT = 6000;
+
+    @BindView(R.id.recieve_text)
+    TextView receivedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server);
+        ButterKnife.bind(this);
+
+        updateConversationHandler = new Handler();
+        serverThread = new Thread(new ServerThread());
+        serverThread.start();
+    }
+
+    class ServerThread implements Runnable {
+
+        @Override
+        public void run() {
+            Socket socket = null;
+            try {
+                serverSocket = new ServerSocket(SERVERPORT);
+                Log.i(LOG_TAG, "serverSocket, " + serverSocket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    socket = serverSocket.accept();
+                    Log.i(LOG_TAG, "socket client server, " + socket);
+                    CommunicationThread commThread = new CommunicationThread(socket);
+                    new Thread(commThread).start();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class CommunicationThread implements Runnable {
+        private Socket clientSocket;
+        private BufferedReader input;
+
+        public CommunicationThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+            try {
+                input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String readData = input.readLine();
+                    Log.i(LOG_TAG, "read data, " + readData);
+                    updateConversationHandler.post(new updateUIThread(readData));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+    class updateUIThread implements Runnable {
+        private String msg;
+
+        public updateUIThread(String msg) {
+            this.msg = msg;
+        }
+
+        @Override
+        public void run() {
+         receivedText.setText("Client Says: " + msg);
+        }
+    }
+}
